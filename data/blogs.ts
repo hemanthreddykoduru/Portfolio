c76b7eeb91ba1cd4c78cd4f9cc44e2feaa197a38
Hemanth Reddy <hemanthreddykoduru2005@gmail.com>
2026-06-11 06:51:24 +0530

    content: Update Razorpay blog post title

--- data/blogs.ts
+++ data/blogs.ts
@@ -17,7 +17,7 @@ export type Blog = {
 export const blogs: Blog[] = [
   {
     slug: "bringing-razorpay-to-insforge",
-    title: "Bringing Razorpay to InsForge: Building a Production-Grade Payment Flow",
+    title: "Bringing Razorpay to InsForge: Building a Production-Grade Payment Flow for a Y Combinator-Backed Open Source Platform",
     date: "08 Jun 2026",
     readTime: "7 minutes",
     author: {
